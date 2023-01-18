70aa18add5dd7e6d8e3bf5857fcacb2bc386b70a
Ruslan <zzzlyka221@gmail.com>
2023-01-18 22:25:03 +0500

    about page completed & minor fixes

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.mainPage, name='mainPage'),
     path('post/<int:id>/', views.post, name='post'),
-    path('results', views.search, name='search')
+    path('results', views.search, name='search'),
+    path('about/', views.about, name='about')
 ]
--- post/views.py
+++ post/views.py
@@ -45,3 +45,7 @@ def search(request):
 
     return HttpResponse(template.render(context, request))
 
+def about(request):
+    template = loader.get_template('post/about.html')
+
+    return HttpResponse(template.render({}, request))
