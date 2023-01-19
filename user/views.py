61696dccc918858853df4d9f782c6921dae04d64
Ruslan <zzzlyka221@gmail.com>
2023-01-19 22:35:02 +0500

    registration & log in completed, template changed

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('mainPage')
+    else:
+        form = RegistrationForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, 'registration/sign_up.html', context)
