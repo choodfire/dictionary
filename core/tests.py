c132d15349a1e572cf5a7cb588d139ce5f178986
Ruslan <zzzlyka221@gmail.com>
2023-01-26 19:15:15 +0500

    core tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class Response200Test(TestCase):
+    def setUp(self):
+        pass
+
+    def testWithoutParams(self):
+        response = self.client.get(reverse('core:mainPage'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('core:about'))
+        self.assertEqual(response.status_code, 200)
+
